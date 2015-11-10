3e22d80832ccdc05d4b8b49c7917071b7c6a8141
Irpan Budiana <irpan.budiana@kereta-api.co.id>
2015-11-10 10:37:30 +0700

    Membuat Window Sederhana

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,29 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 /**
  * Created by Irpan Budiana on 11/10/2015.
  */
-public class Main {
+public class Main extends Application {
+    Button button;
     public static void main(String[] args) {
-        System.out.println("Hello");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Latihan JavaFX");
+        button = new Button();
+        button.setText("Klik saya dong!");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
